3351904e066a2dfbaefaa6ab5944482c3c137d88
Saroj Tripathi <34992534+saroj22322@users.noreply.github.com>
2020-07-03 22:24:38 +0200

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-ssh root@104.236.57.112 <<EOF
   cd djtrump
   git pull
-  source /opt/envs/djtrump/bin/activate
+  source /root/djtrumpenv/bin/activate
   pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djtrump
